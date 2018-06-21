4233b325440c94617c78fa8348686634617427b4
abreternitz <aliceburian@gmail.com>
2018-06-21 21:08:27 +0000

    automatically backed up by learn

--- choy.rb
+++ choy.rb
@@ -58,4 +58,86 @@ elsif birthmonth == "july"||birthmonth=="July"
   puts "As a vegetable, Middle Choys are average tasting! 
 You are ambitious and creative, however, you tend to be 
 overprotective towards those who you love.".italic
-  puts "You are most in your element when making beautiful art!".colorize(:light_green)
+  puts "You are most in your element when making beautiful art!".colorize(:light_green)  
+elsif birthmonth == "august"||birthmonth=="August"
+  puts "You are:"
+  puts "Brain Choy".colorize(:magenta)
+  puts "As a vegetable, Brain Choys are filled with flavor! 
+You are successful and loving, however, you can be too delicate.".italic
+  puts "You are most in your element when learning!".colorize(:magenta)
+  
+elsif birthmonth == "september"||birthmonth=="September"
+  puts "You are:"
+  puts "Gai Choy".colorize(:blue)
+  puts "As a vegetable, Gai Choys have a spicy taste! 
+You are elegant and charming, however, you are a hopeless romantic.".italic
+  puts "You are most in your element when going on dates!".colorize(:blue)
+   
+elsif birthmonth == "october"||birthmonth=="October"
+  puts "You are:"
+  puts "Napa Choy/Cabbage".colorize(:cyan)
+  puts "As a vegetable, Napa Choys have a crisp taste! 
+You are Optimistic and Enthusiastic, however, you tend be restless.".italic
+  puts "You are most in your element when traveling!".colorize(:cyan)
+   
+elsif birthmonth == "november"||birthmonth=="November"
+  puts "You are:"
+  puts "Chilly Choy".colorize(:light_blue)
+  puts "As a vegetable, Chilly Choys have a freezing flavor! 
+You are committed and strong, however, you tend to be hot-headed.".italic
+  puts "You are most in your element when ice skating!".colorize(:light_blue)
+  
+elsif birthmonth == "december"||birthmonth=="December"
+  puts "You are:"
+  puts "Baby Bok Choy".colorize(:red)
+  puts "As a vegetable, Baby Bok Choys are a good-smelling vegetable! 
+You are confident and traditional, however, you can be very pessimistic.".italic
+  puts "You are most in your element when discovering the world around you!".colorize(:red)
+  end
+ 
+  sleep (8)
+  
+puts "Could you remind me what your Choy type is? I suddenly forgot! I'm so forgetful.".colorize(:green)
+choytype=gets.chomp.upcase
+
+  puts "Wow, I can't believe that you got the best type of Choy! 
+Another element to the Choy zodiac is the Choymate. 
+A Choymate is the best love match for you individually!".colorize(:green)
+
+puts "Would you like to discover who your destined Choymate is?".colorize(:green).italic
+   
+   answer=gets.chomp.upcase
+ 
+if answer =="YES"
+
+  choymate = {"NEW CHOY"=>"Napa Choy; This pairing is meant to be because their 
+characteristics such as being restless, optimistic and enthusiatic compliment your choy well. 
+They also have a very crisp taste!",
+  
+    "CHILLY CHOY"=>"Yau Choy; This pairing is meant to be because their 
+characteristics such as being quick-witted, emotionally stable and indesicive compliment your choy well. 
+They also have a dark green coloring! ",
+    
+    "HEART CHOY" => "Brain Choy; This pairing is meant to be because their 
+characteristics such as being delicate, loving and successful compliment your choy well. 
+They are also filled with flavor!",
+    
+    "SPRING CHOY"=>"Shanghai Bok Choy;This pairing is meant to be because their 
+characteristics such as being energetic, a natural leader and having big dreams compliment your choy well.
+They also have a light green coloring!",
+    
+    "BRAIN CHOY"=>"Heart Choy; This pairing is meant to be because their 
+characteristics such as being compassionate, nurturing and overly emotional compliment your choy well.
+They also have a hearty taste!",
+    
+    "YAU CHOY"=>"Chilly Choy; This pairing is meant to be because their 
+characteristics such as being comitted, strong and hot headed compliment your choy well. 
+They also have a freezing flavor!",
+    
+    "GAI CHOY"=>"Baby Bok Choy; This pairing is meant to be because their 
+characteristics such as being confident, pessimistic and traditional compliment your choy well. 
+They also smell very fresh!",
+    
+    "NAPA CHOY"=>"New Choy; This pairing is meant to be because their 
+characteristics such as being unique, intelligent and having high expectations compliment your choy well. 
+They are also a very fresh veggie!",
